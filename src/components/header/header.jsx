b921230ec787f53050687a7276f8e2d07019a9a6
tenmtech <tenmtech@gmail.com>
2023-05-23 16:08:03 +0300

    changed to tailwind

---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <img src="/images/logo.png" alt="logo" className=" " />
+    </div>
+  );
+};
+
+export default Header;
