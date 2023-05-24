ac9412fbfe3418362b7f2e35b55a7f293eda8cdb
tenmtech <tenmtech@gmail.com>
2023-05-24 13:49:12 +0300

    carusel added

--- src/layout/home.jsx
+++ src/layout/home.jsx
@@ -3,12 +3,14 @@ import Navbar from "../components/navbar/navbar";
 
 import Presentation from "../components/presentation/presentation";
 import FirstDescription from "../components/firstDescription/FirstDescription";
+import Tort from "../components/Tort/Tort";
 const home = () => {
   return (
     <div className="container mx-auto  px-8 xl:px-32">
       <Navbar />
       <Presentation />
       <FirstDescription />
+      <Tort />
     </div>
   );
 };
