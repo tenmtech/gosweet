fc871064f7759fa2c89114b6d52e70766c502074
tenmtech <tenmtech@gmail.com>
2023-05-25 14:32:58 +0300

    new fiexs

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,19 +5,18 @@ import { FaTty, FaEnvelope, FaCity } from "react-icons/fa";
 function Contact() {
   return (
     <section className="">
-      <div></div>
-      <div className="flex gap-4 md:text-xl">
+      <div className="flex sm:py-2 gap-4 md:text-xl place-items-center">
         <FaTty />
         <h1>Telefon: +40 755 384 117</h1>
       </div>
-      <div className="flex gap-4 md:text-xl">
+      <div className="flex sm:py-2 gap-4 md:text-xl place-items-center">
         <FaEnvelope />
         <a href="mailto:comenzi@gosweet.ro">
           <h1>comenzi@gosweet.ro</h1>
         </a>
       </div>
-      <div className="flex gap-4 md:text-xl">
-        <FaCity />
+      <div className="flex sm:py-2 gap-4 md:text-xl ">
+        <FaCity className="my-1" />
         <h1>
           Bd. G.al Vasile Milea nr.37 <br />
           (colț cu Vasile Aaron)
