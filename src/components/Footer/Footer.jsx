b8cc722362f9adf639b72b8b7a938103d8507f1a
tenmtech <tenmtech@gmail.com>
2023-05-25 14:23:18 +0300

    fixed year in footer to auto update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,7 +11,7 @@ import "../../index.css";
 function Footer() {
   return (
     <section className="flex justify-between place-items-center center px-2 bg-[#D1A4A9]/60 ">
-      <span className="text-sm">© Copyright SynaByte 2023</span>
+      <span className="text-sm">© Copyright {new Date().getFullYear()}</span>
       <div className="flex text-xl p-2 gap-2 align-middle text-slate-700">
         <FaPhoneSquareAlt />
         <FaInstagramSquare />
